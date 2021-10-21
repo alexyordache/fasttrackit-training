215043fee22a156b085ce22d8ea02a41722f9ede
Alexandru Iordache <alexandru.iordache@itiviti.com>
2021-10-21 16:18:50 +0300

    [Course 18] Homework 3) University asynchronous Notifications and Messages

---
+++ university-notification/src/test/java/org/fasttrackit/universitynotification/UniversityNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.universitynotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
